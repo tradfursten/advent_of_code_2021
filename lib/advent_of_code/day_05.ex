c5d16cd038471b54bbc1280e6032765b46feda0d
adam.sernheim <adam@sernheim.com>
2021-12-05 21:17:53 +0100

    Day 6

--- lib/advent_of_code/day_05.ex
+++ lib/advent_of_code/day_05.ex
@@ -1,7 +1,68 @@
 defmodule AdventOfCode.Day05 do
   def part1(args) do
+    args
+    |> String.trim()
+    |> String.split("\n")
+    |> Enum.map(&parse/1)
+    |> Enum.map(&expand/1)
+    |> Enum.reduce(%{}, &create_grid/2)
+    |> Map.values()
+    |> Enum.filter(fn i -> i > 1 end)
+    |> Enum.count
+  end
+
+  defp parse(line) do
+    line
+    |> String.split(" -> ")
+    |> Enum.map(fn l -> 
+      l 
+      |> String.split(",")
+      |> Enum.map(&String.to_integer/1)
+      end)
+  end
+
+  defp expand([[x, y], [x, z]]) do
+    for i <- y..z do
+      {x, i}
+    end
+  end
+  defp expand([[x, y], [z, y]]) do
+    for i <- x..z do
+      {i, y}
+    end
+  end
+  defp expand(_), do: []
+
+  defp create_grid([], acc), do: acc
+  defp create_grid([{x, y} | tail], acc) do
+    create_grid(tail, Map.update(acc, {x, y}, 1, fn i -> i + 1 end))
   end
 
   def part2(args) do
+    args
+    |> String.trim()
+    |> String.split("\n")
+    |> Enum.map(&parse/1)
+    |> Enum.map(&expand_dig/1)
+    |> Enum.reduce(%{}, &create_grid/2)
+    |> Map.values()
+    |> Enum.filter(fn i -> i > 1 end)
+    |> Enum.count
+  end
+
+  defp expand_dig([[x, y], [x, z]]) do
+    for i <- y..z do
+      {x, i}
+    end
+  end
+
+  defp expand_dig([[x, y], [z, y]]) do
+    for i <- x..z do
+      {i, y}
+    end
+  end
+
+  defp expand_dig([[x, y], [z, w]]) do
+    Enum.zip([x..z, y..w])
   end
 end
--- test/advent_of_code/day_05_test.exs
+++ test/advent_of_code/day_05_test.exs
@@ -3,18 +3,39 @@ defmodule AdventOfCode.Day05Test do
 
   import AdventOfCode.Day05
 
-  @tag :skip
   test "part1" do
-    input = nil
+    input = """
+0,9 -> 5,9
+8,0 -> 0,8
+9,4 -> 3,4
+2,2 -> 2,1
+7,0 -> 7,4
+6,4 -> 2,0
+0,9 -> 2,9
+3,4 -> 1,4
+0,0 -> 8,8
+5,5 -> 8,2
+"""
     result = part1(input)
-
+    IO.inspect(result)
     assert result
   end
 
-  @tag :skip
   test "part2" do
-    input = nil
+    input = """
+0,9 -> 5,9
+8,0 -> 0,8
+9,4 -> 3,4
+2,2 -> 2,1
+7,0 -> 7,4
+6,4 -> 2,0
+0,9 -> 2,9
+3,4 -> 1,4
+0,0 -> 8,8
+5,5 -> 8,2
+"""
     result = part2(input)
+    IO.inspect(result)
 
     assert result
   end
